5e1398d2cb36768b988fa6f303b1098a1f5c7a49
Frenzel <frenzel01@gmail.com>
2019-05-10 20:24:54 +0700

    transaction

---
+++ src/main/java/com/alpajazel/bookrrow/controller/TransactionController.java
@@ -0,0 +1,14 @@
+package com.alpajazel.bookrrow.controller;
+
+import com.alpajazel.bookrrow.models.Book;
+import com.alpajazel.bookrrow.models.Consumer;
+import com.alpajazel.bookrrow.models.Transaction;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+@RequestMapping("/book")
+public class TransactionController {
+
+    
+}
--- src/main/java/com/alpajazel/bookrrow/models/Transaction.java
+++ src/main/java/com/alpajazel/bookrrow/models/Transaction.java
@@ -5,6 +5,33 @@ public class Transaction {
     private Book book;
     private Consumer borrower;
 
+    public Transaction(int id, Book book, Consumer borrower) {
+        this.id = id;
+        this.book = book;
+        this.borrower = borrower;
+    }
 
+    public int getId() {
+        return id;
+    }
 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Consumer getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(Consumer borrower) {
+        this.borrower = borrower;
+    }
 }
