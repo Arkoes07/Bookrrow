7661dcf4d1f884690fd61264997eed6842df0491
Arkoes07 <m.alwi@ui.ac.id>
2019-05-17 11:02:46 +0700

    add javadoc at BookNotFoundException class

--- src/main/java/com/alpajazel/bookrrow/exceptions/BookNotFoundException.java
+++ src/main/java/com/alpajazel/bookrrow/exceptions/BookNotFoundException.java
@@ -1,12 +1,32 @@
 package com.alpajazel.bookrrow.exceptions;
 
+/**
+ * Exception class that thrown to notify that the Book is not found
+ *
+ * @author M. Alwi Sukra
+ * @version 1.0.0
+ * @since 2019-05-16
+ */
 public class BookNotFoundException extends Exception{
     private int book_error;
+
+    /**
+     * Constructor for this exception class
+     *
+     * @param book_input id of the book that doesn't exist or not found
+     * @since 2019-05-16
+     */
     public BookNotFoundException(int book_input) {
         super("Book ID: ");
         this.book_error = book_input;
     }
 
+    /**
+     * Generate meaningful error message
+     *
+     * @return meaningful error message
+     * @since 2019-05-16
+     */
     public String getExMessage(){
         return  "===============ITEM NOT FOUND===============\n"+super.getMessage()+book_error+" not found.\n\n";
     }
